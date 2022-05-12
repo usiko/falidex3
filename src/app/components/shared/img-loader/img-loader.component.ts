ca263a423a0517172bc770a899c85f00834b6498
usiko <61324827+usiko@users.noreply.github.com>
2022-05-12 14:07:37 +0200

    blindcoding - icon error img

--- src/app/components/shared/img-loader/img-loader.component.ts
+++ src/app/components/shared/img-loader/img-loader.component.ts
@@ -1,50 +1,52 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-img-loader',
-    templateUrl: './img-loader.component.html',
-    styleUrls: ['./img-loader.component.scss'],
+	selector: 'app-img-loader',
+	templateUrl: './img-loader.component.html',
+	styleUrls: ['./img-loader.component.scss'],
 })
 export class ImgLoaderComponent implements OnInit {
-    public loading = false;
-    public ownsrc: string;
+	public loading = false;
+	public ownsrc: string;
+	public ownIcon: string;
 
-    @Input() set src(src: string) {
-        this.loading = true;
-        //console.log('img change', src, this.errorSrc, this.ownsrc);
-        if (src) {
-            this.ownsrc = src;
-            //console.log('img change', src, this.errorSrc, this.ownsrc);
-        }
-        else {
-            this.ownsrc = this.errorSrc;
-            //console.log('img change', src, this.errorSrc, this.ownsrc);
-        }
-    }
+	@Input() set src(src: string) {
+		this.loading = true;
+		//console.log('img change', src, this.errorSrc, this.ownsrc);
+		if (src) {
+			this.ownsrc = src;
+			//console.log('img change', src, this.errorSrc, this.ownsrc);
+		} else {
+			if (this.errorSrc) {
+				this.ownsrc = this.errorSrc;
+			} else {
+				this.errorIcon = this.ownIcon;
+			}
 
-    get src() {
-        return this.ownsrc;
-    }
-    @Input() errorSrc = '/assets/no-image.png';
+			//console.log('img change', src, this.errorSrc, this.ownsrc);
+		}
+	}
 
-    @Input() objectFit = 'cover';
+	@Input() errorSrc;
 
-    constructor() { }
+	@Input() errorIcon = 'seal-question';
 
-    ngOnInit() { }
+	@Input() objectFit = 'cover';
 
-    imgError() {
-        console.warn('error loading', this.ownsrc);
-        this.loading = false;
-        if (this.ownsrc !== this.errorSrc) {
-            this.ownsrc = this.errorSrc;
-        }
+	constructor() {}
 
-    }
+	ngOnInit() {}
 
-    imgLoaded() {
-        console.log('img loaded', this.ownsrc);
-        this.loading = false;
-    }
+	imgError() {
+		console.warn('error loading', this.ownsrc);
+		this.loading = false;
+		if (this.ownsrc !== this.errorSrc) {
+			this.ownsrc = this.errorSrc;
+		}
+	}
 
+	imgLoaded() {
+		console.log('img loaded', this.ownsrc);
+		this.loading = false;
+	}
 }
