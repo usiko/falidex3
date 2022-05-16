9d33eb2ef665ecb6cbc68fffad605ee882e5b022
usiko <61324827+usiko@users.noreply.github.com>
2022-05-16 12:39:01 +0200

    blindcoding, apply config loading steps

--- src/app/services/data-store/loader/data-loader-store.service.ts
+++ src/app/services/data-store/loader/data-loader-store.service.ts
@@ -15,17 +15,17 @@ import NATRelation from '../../mocks/relations/national.json';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, take, map, mergeMap, delay, tap } from 'rxjs/operators';
 import {
-    IBaseCirculaire,
-    IBaseCirculaireColor,
-    IBaseCollectionData,
-    IBaseColor,
-    IBaseFiliere,
-    IBasePlacement,
-    IBasePosition,
-    IBaseSignification,
-    IBaseSymbol,
-    IBaseSymbolAcessory,
-    IBaseSymbolSens,
+	IBaseCirculaire,
+	IBaseCirculaireColor,
+	IBaseCollectionData,
+	IBaseColor,
+	IBaseFiliere,
+	IBasePlacement,
+	IBasePosition,
+	IBaseSignification,
+	IBaseSymbol,
+	IBaseSymbolAcessory,
+	IBaseSymbolSens,
 } from 'src/app/models/base-data-models';
 import { StoreService } from '../base-store/store.service';
 import { ILoadingBarState } from '../../../models/global.model';
@@ -34,242 +34,211 @@ import { IRelationData } from 'src/app/models/base-relations.models';
 import { ILoadingSteps } from '../../../models/config.model';
 
 @Injectable({
-    providedIn: 'root',
+	providedIn: 'root',
 })
 export class DataLoaderStoreService {
-    private delay = 0;
-    private loadingSteps: ILoadingSteps = [];
-    constructor(private store: StoreService, private event: EventService, private config) {}
+	private delay = 0;
+	private loadingSteps: ILoadingSteps = [];
+	constructor(private store: StoreService, private event: EventService, private config) {}
 
-    loadData(): void {
-        const loadingSteps = this.config.getConfig().loadingSteps;
-        const numberOfSteps = 13;
-        of(null)
-            .pipe(
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadPlacements(), this.store.placements$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0,
-                        buffer: 0.08,
-                        message: 'Accrochage des grelots',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadSymbolsAccessory(), this.store.symbolesAccessories$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.08,
-                        buffer: 0.16,
-                        message: 'Tocardisation des tocards',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadPositions(), this.store.positions$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.16,
-                        buffer: 0.3,
-                        message: 'Cuisson de la faluche',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadCirculaires(), this.store.circulaires$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.3,
-                        buffer: 0.38,
-                        message: 'Drama satin/velours',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadCirculairesColors(), this.store.circulairesColors$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.38,
-                        buffer: 0.46,
-                        message: 'Coloriage des rubans',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadColors(), this.store.colors$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.46,
-                        buffer: 0.54,
-                        message: 'Couture des circulaires',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadFilieres(), this.store.filieres$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.54,
-                        buffer: 0.62,
-                        message: 'Décuvage des PMs',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadSymbols(), this.store.symboles$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.62,
-                        buffer: 0.7,
-                        message: 'Décernement des singes',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadSymbolsSens(), this.store.symbolesSens$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.7,
-                        buffer: 0.78,
-                        message: 'Prepatation des pichets',
-                    });
-                }),
-                mergeMap(() => {
-                    return this.dispactIntoSubject(this.loadSignifications(), this.store.significations$);
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.78,
-                        buffer: 0.86,
-                        message: 'Tarte aux pommes',
-                    });
-                }),
+	loadData(): void {
+		this.loadingSteps = this.config.getConfig().loadingSteps;
+		const numberOfSteps = 12;
+		const currentStep = 1;
+		of(null)
+			.pipe(
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadPlacements(), this.store.placements$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadSymbolsAccessory(), this.store.symbolesAccessories$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadPositions(), this.store.positions$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadCirculaires(), this.store.circulaires$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadCirculairesColors(), this.store.circulairesColors$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadColors(), this.store.colors$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadFilieres(), this.store.filieres$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadSymbols(), this.store.symboles$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadSymbolsSens(), this.store.symbolesSens$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
+				mergeMap(() => {
+					return this.dispactIntoSubject(this.loadSignifications(), this.store.significations$);
+				}),
+				tap(() => {
+					this.displayStep();
+				}),
 
-                mergeMap(() => {
-                    return this.loadRelations();
-                }),
-                tap(() => {
-                    this.displayLoading({
-                        enable: true,
-                        value: 0.86,
-                        buffer: 1,
-                        message: 'Lecture du code',
-                    });
-                })
-            )
-            .subscribe(() => {
-                console.log('colleciton loaded');
-                this.displayLoading({
-                    enable: true,
-                    value: 1,
-                    buffer: 1,
-                    message: '"La fal c`est un truc serieux"',
-                });
-                setTimeout(() => {
-                    this.displayLoading({
-                        enable: false,
-                        value: 0,
-                        buffer: 0,
-                        message: '',
-                    });
-                }, 750);
-            });
-    }
+				mergeMap(() => {
+					return this.loadRelations();
+				}),
+				tap(() => {
+					this.displayStep();
+				})
+			)
+			.subscribe(() => {
+				console.log('colleciton loaded');
+				this.displayLoading({
+					enable: true,
+					value: 1,
+					buffer: 1,
+					message: '"La fal c`est un truc serieux"',
+				});
+				setTimeout(() => {
+					this.displayLoading({
+						enable: false,
+						value: 0,
+						buffer: 0,
+						message: '',
+					});
+				}, 750);
+			});
+	}
 
-    private getStepMessage(stepNumber: number)
-    {
-        if (this.loadingSteps.length === 0) {
-            return null;
-        }
-        else {
-            if (!this.loadingSteps[stepNumber])
-            {
-                return this.loadingSteps[this.loadingSteps.length - 1];
-            }
-            else {
-                return this.loadingSteps[stepNumber];
-            }
-        }
-    }
+	private getStepMessage(stepNumber: number) {
+		if (this.loadingSteps.length === 0) {
+			return null;
+		} else {
+			if (!this.loadingSteps[stepNumber]) {
+				return this.loadingSteps[this.loadingSteps.length - 1];
+			} else {
+				return this.loadingSteps[stepNumber - 1];
+			}
+		}
+	}
 
-    private loadRelations(): Observable<IRelationData[]> {
-        return of([TLNRelation, NATRelation]).pipe(
-            delay(750),
-            map((items) => {
-                console.log('relation loaded');
-                this.store.dataRelations$.next(items);
-                console.log('relation selected');
-                this.store.currentDataRelations$.next(items[0]);
-                return items;
-            })
-        );
-    }
+	private displayStep(currentStep: number, numberOfSteps: number) {
+		const nextvalue = (1 / numberOfSteps) * currentStep;
+		const lastvalue = (1 / numberOfSteps) * currentStep - 1;
+		if (nextvalue > 1) {
+			nextvalue = 1;
+		}
+		if (lastvalue > 1) {
+			lastvalue = 1;
+		}
+		this.displayLoading({
+			enable: !!this.getStepMessage(currentStep),
+			value: lastvalue,
+			buffer: nextvalue,
+			message: this.getStepMessage(currentStep),
+		});
+		if (nextvalue === 1) {
+			setTimeout(() => {
+				this.displayLoading({
+					enable: false,
+					value: 0,
+					buffer: 0,
+					message: '',
+				});
+			}, 750);
+		}
+	}
 
-    private loadCirculaires(): Observable<IBaseCirculaire[]> {
-        return of(Circulaires);
-    }
+	private loadRelations(): Observable<IRelationData[]> {
+		return of([TLNRelation, NATRelation]).pipe(
+			delay(750),
+			map((items) => {
+				console.log('relation loaded');
+				this.store.dataRelations$.next(items);
+				console.log('relation selected');
+				this.store.currentDataRelations$.next(items[0]);
+				return items;
+			})
+		);
+	}
 
-    private loadCirculairesColors(): Observable<IBaseCirculaireColor[]> {
-        return of(CirculairesColors);
-    }
+	private loadCirculaires(): Observable<IBaseCirculaire[]> {
+		return of(Circulaires);
+	}
 
-    private loadColors(): Observable<IBaseColor[]> {
-        return of(Colors);
-    }
+	private loadCirculairesColors(): Observable<IBaseCirculaireColor[]> {
+		return of(CirculairesColors);
+	}
 
-    private loadFilieres(): Observable<IBaseFiliere[]> {
-        return of(Filieres);
-    }
-    private loadSignifications(): Observable<IBaseSignification[]> {
-        return of(Significations);
-    }
-    private loadSymbols(): Observable<IBaseSymbol[]> {
-        return of(Symbols);
-    }
-    private loadSymbolsSens(): Observable<IBaseSymbolSens[]> {
-        return of(SymbolsSens);
-    }
-    private loadSymbolsAccessory(): Observable<IBaseSymbolAcessory[]> {
-        return of(SymbolAccessory);
-    }
-    private loadPlacements(): Observable<IBasePlacement[]> {
-        return of(Placements);
-    }
-    private loadPositions(): Observable<IBasePosition[]> {
-        return of(Positions);
-    }
+	private loadColors(): Observable<IBaseColor[]> {
+		return of(Colors);
+	}
 
-    private dispactIntoSubject(obs: Observable<IBaseCollectionData[]>, subject: BehaviorSubject<IBaseCollectionData[]>) {
-        return obs.pipe(
-            take(1),
-            delay(this.delay),
-            map((items) => {
-                console.log(items.length, 'items loaded');
-                subject.next(items);
-                return items;
-            }),
-            //add delay here
-            catchError((error) => {
-                console.error(error);
-                return of([]);
-            })
-        );
-    }
+	private loadFilieres(): Observable<IBaseFiliere[]> {
+		return of(Filieres);
+	}
+	private loadSignifications(): Observable<IBaseSignification[]> {
+		return of(Significations);
+	}
+	private loadSymbols(): Observable<IBaseSymbol[]> {
+		return of(Symbols);
+	}
+	private loadSymbolsSens(): Observable<IBaseSymbolSens[]> {
+		return of(SymbolsSens);
+	}
+	private loadSymbolsAccessory(): Observable<IBaseSymbolAcessory[]> {
+		return of(SymbolAccessory);
+	}
+	private loadPlacements(): Observable<IBasePlacement[]> {
+		return of(Placements);
+	}
+	private loadPositions(): Observable<IBasePosition[]> {
+		return of(Positions);
+	}
 
-    private displayLoading(loadingState: ILoadingBarState) {
-        console.log('displayLoading', loadingState);
-        this.event.publish('loadingBarState', loadingState);
-    }
+	private dispactIntoSubject(obs: Observable<IBaseCollectionData[]>, subject: BehaviorSubject<IBaseCollectionData[]>) {
+		return obs.pipe(
+			take(1),
+			delay(this.delay),
+			map((items) => {
+				console.log(items.length, 'items loaded');
+				subject.next(items);
+				return items;
+			}),
+			//add delay here
+			catchError((error) => {
+				console.error(error);
+				return of([]);
+			})
+		);
+	}
+
+	private displayLoading(loadingState: ILoadingBarState) {
+		console.log('displayLoading', loadingState);
+		this.event.publish('loadingBarState', loadingState);
+	}
 }
