6ab7815d4763cd029e723402846adce9464c1de0
usiko <61324827+usiko@users.noreply.github.com>
2022-05-24 16:29:35 +0200

    blindcoding circulaire details

--- src/app/components/block/details/circulaire/circulaire-details-block/circulaire-details-block.component.ts
+++ src/app/components/block/details/circulaire/circulaire-details-block/circulaire-details-block.component.ts
@@ -9,6 +9,47 @@ export class CirculaireDetailsBlockComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild(IonSlides) slide: IonSlides;
+
+  @Input() circualaire$: BehaviorSubject<ICirculaire>;
+  slideOptions = {
+      autoplay: false,
+      pagination: false,
+  };
+  slideIndex = 0;
+
   ngOnInit() {}
+
+  ngAfterViewInit(): void {
+      if (this.slide) {
+          this.slide.getActiveIndex().then((num) => {
+              if (num !== this.slideIndex) {
+                  this.slideIndex = num;
+              }
+          });
+      }
+  }
+
+  slidesChange(data) {
+      if (this.slide) {
+          this.slide.getActiveIndex().then((num) => {
+              if (num !== this.slideIndex) {
+                  this.slideIndex = num;
+              }
+          });
+      }
+  }
+
+  slideprevious() {
+      if (this.slide) {
+          this.slide.slidePrev();
+      }
+  }
+
+  slidenext() {
+      if (this.slide) {
+          this.slide.slideNext();
+      }
+  }
 
 }
