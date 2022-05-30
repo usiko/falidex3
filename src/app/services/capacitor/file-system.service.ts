221b8c82f0804d3811aeb9423b648f788a66597c
usiko <61324827+usiko@users.noreply.github.com>
2022-05-30 14:57:55 +0200

    blindcoding fileSystel service

---
+++ src/app/services/capacitor/file-system.service.ts
@@ -0,0 +1,28 @@
+import { Capacitor, Plugins, CameraResultType, FilesystemDirectory } from '@capacitor/core';
+const { Camera, Filesystem } = Plugins;
+export class FileSystemService {
+	private delay = 0;
+	constructor(private store: StoreService, private event: EventService, private picturePrelaoder: PicturePreloaderService) {}
+
+	savePicture(path: string, name: string) {
+		const fileName = name + '.jpg';
+		Filesystem.readFile({
+			path: path,
+		}).then(() => {
+			Filesystem.getUri({
+				directory: FilesystemDirectory.Data,
+				path: fileName,
+			}).then(
+				(result) => {
+					let path = Capacitor.convertFileSrc(result.uri);
+					console.log(path);
+				},
+				(err) => {
+					console.log(err);
+				}
+			);
+		});
+	}
+
+	retreivePicture() {}
+}
