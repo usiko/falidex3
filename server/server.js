3ecf14bdb0a786bd22155f975e908218d0a1d05d
quentin <quentin.usiko@gmail.com>
2022-06-29 01:08:07 +0200

    express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var htmlPath = path.join(__dirname, '../www');
+console.log(htmlPath);
+app.use(express.static(htmlPath));
+
+var server = app.listen(process.env.PORT || 8080, function() {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('listening on http://' + host + ':' + port + '/');
+});
