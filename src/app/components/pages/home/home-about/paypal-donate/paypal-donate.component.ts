b6abcb7928fef93944ca3535c36e1cd043f00e85
quentin <quentin.usiko@gmail.com>
2022-07-06 12:50:52 +0200

    paypal donate component

---
+++ src/app/components/pages/home/home-about/paypal-donate/paypal-donate.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paypal-donate',
+  templateUrl: './paypal-donate.component.html',
+  styleUrls: ['./paypal-donate.component.scss'],
+})
+export class PaypalDonateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
