12dc4317610d26d521fa561465c7ab0d866d2e24
quentin <quentin.usiko@gmail.com>
2022-07-27 16:55:54 +0200

    redirect /

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ var app = express();
 var htmlPath = path.join(__dirname, '../www');
 console.log(htmlPath);
 app.use(express.static(htmlPath));
-
+app.all('*', (req, res, next) => {
+    res.redirect('/')
+})
 var server = app.listen(process.env.PORT || 8080, function() {
     var host = 'localhost';
     var port = server.address().port;
