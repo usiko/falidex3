8e26303f4ba54ea1877f6cadf2576cb2471f92a7
quentin <quentin.usiko@gmail.com>
2026-05-09 21:29:47 +0200

    secret login dev mode

--- src/app/components/pages/home/home-about/home-about.component.ts
+++ src/app/components/pages/home/home-about/home-about.component.ts
@@ -1,20 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { IonCard, IonCardContent, IonText } from "@ionic/angular/standalone";
+import { IonCard, IonCardContent, IonText, ModalController } from "@ionic/angular/standalone";
 import { AppConfigService } from 'src/app/services/config/app.config.service';
 import { PaypalDonateComponent } from "./paypal-donate/paypal-donate.component";
 import { environment } from 'src/environments/environment';
+import { SecretLoginComponent } from '../../../secret-login/secret-login.component';
 
 @Component({
     selector: 'app-home-about',
     templateUrl: 'home-about.component.html',
     styleUrls: ['home-about.component.scss'],
-    imports: [IonCard, IonCardContent, IonText, PaypalDonateComponent,CommonModule],
+    imports: [IonCard, IonCardContent, IonText, PaypalDonateComponent, CommonModule],
 })
 export class HomeAbout {
     private configService = inject(AppConfigService);
+    private modalCtrl = inject(ModalController);
     public showDonate = false;
+    private clickTimestamps: number[] = [];
+    
     ngOnInit(): void {
         this.showDonate = !!environment.donate;
+    }
+
+    onCardClick() {
+        const now = Date.now();
+        this.clickTimestamps.push(now);
+        
+        // Garder seulement les clics des 10 dernières secondes
+        this.clickTimestamps = this.clickTimestamps.filter(
+            timestamp => now - timestamp <= 10000
+        );
+        
+        // Si 10 clics ou plus en moins de 10 secondes
+        if (this.clickTimestamps.length >= 20) {
+            this.openSecretLogin();
+            // Réinitialiser le compteur
+            this.clickTimestamps = [];
+        }
+    }
+
+    async openSecretLogin() {
+        const modal = await this.modalCtrl.create({
+            component: SecretLoginComponent
+        });
+        
+        await modal.present();
     }
 }
