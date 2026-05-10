628348fea7a7b14887bcaf120b7294b5fa7a279f
quentin <quentin.usiko@gmail.com>
2026-05-10 14:00:17 +0200

    update model

--- src/app/models/base-relations.models.ts
+++ src/app/models/base-relations.models.ts
@@ -2,6 +2,7 @@ import { IBaseCodeSpe } from './base-data-models';
 import { ICodeSpe } from './linked-data-models';
 
 export interface IRelationItem {
+    id:string;
     placementId?: string;
     positionId?: string;
     filiereId?: string;
@@ -11,7 +12,11 @@ export interface IRelationItem {
     circulaireId?: string;
     significationId?: string;
     spe?: boolean;
+    absent?: boolean;
+    blame?: boolean;
     note?: string;
+    created_at?:Date;
+    updated_at?:Date;
 }
 
 /*export interface IRelationContentSpe {
@@ -26,7 +31,13 @@ export interface IRelationData {
     name: string;
     id: string;
     annee: number;
-    default: boolean;
+    default?: boolean;
     relations: IRelationItem[];
     specificites?: IBaseCodeSpe[];
+    ville?:string;
+    visible?:boolean;
+    national:boolean;
+    last_update?:Date;
+    created_at?:Date;
+
 }
